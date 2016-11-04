108342053ba6b3dc48cafe9a6067f9f1faf11d05
rumman0786 <rumman.ashraf@gmail.com>
2016-11-05 00:15:54 +0600

    Initail Commit

---
+++ src/main/java/com/darkace/javabrains/DrawingApp.java
@@ -0,0 +1,11 @@
+package com.darkace.javabrains;
+
+/**
+ * Created by rumman on 11/5/16.
+ */
+public class DrawingApp {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        triangle.draw();
+    }
+}
---
+++ src/main/java/com/darkace/javabrains/Triangle.java
@@ -0,0 +1,11 @@
+package com.darkace.javabrains;
+
+/**
+ * Created by rumman on 11/5/16.
+ */
+public class Triangle {
+
+    public void draw() {
+        System.out.println("Triangle Drawn");
+    }
+}
