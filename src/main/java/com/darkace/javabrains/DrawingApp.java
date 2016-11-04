558eb422093f2c0458a2a6e87d3ff85f82d524d8
rumman0786 <rumman.ashraf@gmail.com>
2016-11-05 00:36:31 +0600

    Bean Property Set from xml config

--- src/main/java/com/darkace/javabrains/DrawingApp.java
+++ src/main/java/com/darkace/javabrains/DrawingApp.java
@@ -2,6 +2,8 @@ package com.darkace.javabrains;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 /**
@@ -9,8 +11,9 @@ import org.springframework.core.io.FileSystemResource;
  */
 public class DrawingApp {
     public static void main(String[] args) {
-        BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-        Triangle triangle = (Triangle) beanFactory.getBean("triangle");
+//        BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangle = (Triangle) context.getBean("triangle");
         triangle.draw();
     }
 }
--- src/main/java/com/darkace/javabrains/Triangle.java
+++ src/main/java/com/darkace/javabrains/Triangle.java
@@ -5,7 +5,18 @@ package com.darkace.javabrains;
  */
 public class Triangle {
 
-    public void draw() {
-        System.out.println("Triangle Drawn");
+    private String type;
+
+    public String getType() {
+        return type;
     }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void draw() {
+        System.out.println("Triangle Drawn of type: " + type);
+    }
+
 }
